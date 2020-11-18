8570f728055d616b78d9a573260be05f836ca5f9
Sam <Lagicrus@users.noreply.github.com>
2020-11-18 03:34:29 +0000

    Change behaviour

--- tools.py
+++ tools.py
@@ -5,13 +5,13 @@ from discord.ext import commands
 
 def test_username(nickname: str, ctx: commands.Context) -> list:
     errors = []
-    if len(nickname) < 6:
-        errors.append("Your username is too short. It need to be at least 6 characters.")
-    elif len(nickname) > 32:
-        errors.append("Your username is too long. It needs to be under 32 characters.")
-    elif not nickname.isalpha():
-        errors.append("Please only use alphabetical characters in your nickname.")
-    elif not re.match('^[a-zA-Z0-9_]+$', ctx.author.display_name) and nickname == '':
-        errors.append("Your username is invalid. Please choose a nickname with `.join <nickname>`.")
-    return errors
+    string_to_test = ctx.author.username if len(nickname) == 0 else nickname
+    verbal_test = "username" if len(nickname) == 0 else "nickname"
 
+    if len(string_to_test) < 6:
+        errors.append(f"Your {verbal_test} is too short. It need to be at least 6 characters.")
+    if len(string_to_test) > 32:
+        errors.append(f"Your {verbal_test} is too long. It needs to be under 32 characters.")
+    if not string_to_test.isalpha():
+        errors.append(f"Please only use alphabetical characters in your {verbal_test}.")
+    return errors
